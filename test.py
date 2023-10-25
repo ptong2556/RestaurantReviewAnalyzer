c3248214687cc8b7aec6169115116ac3886018b5
lee-youngseo <yl4652@columbia.edu>
2023-10-25 17:50:20 -0400

    add test.py and nyc data extracted

---
+++ test.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import csv
+
+# Define your Yelp API key
+api_key = 'B9cMCVgVcWrQll09BveLmLuOUknpQC85VM8W2jtwird__ZY96umj-s7O4tk8BcUA4NswMvLT5S04yUfK-B_lBhGOUA3qC8BoOx7r9tB7W7M20NIu3uzyfTaJ1JU4ZXYx'
+
+# Define the Yelp API endpoint URL
+url = 'https://api.yelp.com/v3/businesses/search'
+
+# Define the query parameters for the search
+params = {
+    'term': 'restaurants',  # Change to your desired search term
+    'location': 'New York',  # Change to your desired location
+    'limit': 50
+}
+# Define the headers with the Authorization header containing the API key
+headers = {
+    'Authorization': f'Bearer {api_key}',
+    'accept': 'application/json',
+}
+
+nyc = []
+#get 1000
+for y in range(20):
+    location = 'Manhattan, NY'
+    term = "Restaurants"
+    limit = 50
+    offset = 50 * y
+
+    params = {
+        'term': term,
+        'location': location,
+        'limit': 50,
+        'offset': offset
+    }
+
+    response = requests.get(url, headers=headers, params=params)
+    nyc.append(response)
+
+with open('nyc_restaurants.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+    # Write the header
+    writer.writerow(['Business Name', 'Address', 'City', 'Zip Code'])
+    # Write the data from the 'nyc' list
+    writer.writerows(nyc)
+
+
+
+# # Make the GET request
+# response = requests.get(url, params=params, headers=headers)
+# data = None
+# # Check if the request was successful (status code 200)
+# if response.status_code == 200:
+#     data = response.json()  # Parse the JSON response
+#     businesses = data.get('businesses', [])
+#     print(len(businesses))
+#     # Process and use the data as needed
+#     with open('businesses.json', 'w') as json_file:
+#         json.dump(businesses, json_file, indent=4)
+# else:
+#     print(f'Error: {response.status_code}')
+#     print(response.text)
